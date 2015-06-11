004f28bed3bd9445a8161adbbf98f397fa6c13f4
David Pitman <edave@users.noreply.github.com>
2015-06-10 21:03:56 -0600

    Moving install order of GCC

--- setup-converge.sh
+++ setup-converge.sh
@@ -47,3 +47,6 @@ else
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
   \curl -sSL https://get.rvm.io | bash -s stable --ruby --with-default-gems="cocoapods"
 fi
+
+# This is out of order w/ the other brew installs because it takes forever
+brew install gcc
